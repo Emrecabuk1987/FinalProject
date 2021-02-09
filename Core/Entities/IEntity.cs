63fbc3d774fb9bf998c9d25dc3660f1098d609b1
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-02-09 13:05:32 +0200

    6/2/2021 ders sonu

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+
+{
+    //IEntitiy implement eden class bir veri tabanı tablasoudur
+    public interface IEntity
+    {
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //public değil internal yapılır ise katmanlara erişim kapanır
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    } 
+}
