be366e1c3f872c137058c066b9fb7d94e66a3e86
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-03-13 23:06:18 +0300

    15.gün ilk ders sonu hata var çözmen gerekecek

---
+++ Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers
+            (this IServiceCollection serviceCollection, Utilities.IoC.ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(serviceCollection);
+
+            }
+            return Utilities.IoC.ServiceTool.Create(serviceCollection);
+        }
+    }
+}
